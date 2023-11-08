19bedb585679a975d92c3634fff52a99e4d30872
zazed <zazed@shitbook.local>
2023-11-08 19:55:54 +0000

    first commit

---
+++ lua/config/lsps.lua
@@ -0,0 +1,25 @@
+local lsp = require("lspconfig")
+
+local c = vim.lsp.protocol.make_client_capabilities()
+c.textDocument.completion.completionItem.snippetSupport = true
+c.textDocument.completion.completionItem.resolveSupport = {
+  properties = {
+    "documentation",
+    "detail",
+    "additionalTextEdits",
+  },
+}
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities(c)
+
+lsp.dartls.setup({
+  cmd = { "dart", "language-server", "--protocol=lsp" },
+  settings = {},
+})
+
+lsp.ocamllsp.setup({
+  cmd = { "ocamllsp" },
+  filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
+  root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
+  capabilities = capabilities,
+})
